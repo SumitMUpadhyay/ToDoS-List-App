abb7134d38d6fda3f15c5278ebd4af06b650d486
Sumit Upadhyay <sumitpg91@rediffmail.com>
2021-05-05 17:17:31 +0530

    initial commit

---
+++ src/MyComponents/About.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export const About = () => {
+    return (
+        <div>
+            This is an about component
+            <p>lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem 
+                lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem
+            </p>
+        </div>
+    )
+}
